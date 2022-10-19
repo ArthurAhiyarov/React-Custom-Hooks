d08f18cd0ae122cde7fc716b07a9b51188ab8901
Arthur <arakhiyarov@gmail.com>
2022-10-19 15:50:41 +0500

    adjusting the hook and applying it to other components

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,17 +1,19 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
 const useHttp = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
 
-    const sendRequest = async (requestConfig, applyData) => {
+    const sendRequest = useCallback(async (requestConfig, applyData) => {
         setIsLoading(true);
         setError(null);
         try {
             const response = await fetch(requestConfig.url, {
-                method: requestConfig.method,
-                headers: requestConfig.headers,
-                body: JSON.stringify(requestConfig.body),
+                method: requestConfig.method ? requestConfig.method : 'GET',
+                headers: requestConfig.headers ? requestConfig.headers : {},
+                body: requestConfig.body
+                    ? JSON.stringify(requestConfig.body)
+                    : null,
             });
 
             if (!response.ok) {
@@ -20,19 +22,12 @@ const useHttp = () => {
 
             const data = await response.json();
             applyData(data);
-
-            const loadedTasks = [];
-
-            for (const taskKey in data) {
-                loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-            }
-
-            setTasks(loadedTasks);
         } catch (err) {
             setError(err.message || 'Something went wrong!');
         }
         setIsLoading(false);
-    };
+    }, []);
+
     return {
         isLoading,
         error,
